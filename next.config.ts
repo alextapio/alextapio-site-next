eb75fefaf7a357d99cc503b5c06941fb8ff42c9f
alextapio <63820738+alextapio@users.noreply.github.com>
2026-08-27 20:59:20 +0400

    Allow Google Analytics in CSP

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; base-uri 'self'; form-action 'self' https://6db3ffe5.sibforms.com; script-src 'self' 'unsafe-inline' https://sibforms.com; style-src 'self' 'unsafe-inline' https://sibforms.com; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://6db3ffe5.sibforms.com https://sibforms.com; object-src 'none'; frame-ancestors 'none'; upgrade-insecure-requests",
+              "default-src 'self'; base-uri 'self'; form-action 'self' https://6db3ffe5.sibforms.com; script-src 'self' 'unsafe-inline' https://sibforms.com https://www.googletagmanager.com; style-src 'self' 'unsafe-inline' https://sibforms.com; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://6db3ffe5.sibforms.com https://sibforms.com https://www.google-analytics.com https://*.google-analytics.com; object-src 'none'; frame-ancestors 'none'; upgrade-insecure-requests",
           },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "X-Frame-Options", value: "DENY" },
